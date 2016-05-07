9e7e3dba053eb919554d585641b7fd60ca7dbde0
DoHyun Kim <nero.union12@gmail.com>
2016-05-07 20:40:16 +0900

    Profile 모델 추가

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+from teams.models import Tag
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User)
+    profile_image = models.ImageField(upload_to='profile_image')
+    description = models.CharField(max_length=256)
+    tags = models.ManyToManyField(Tag, related_name='users')
+
+    def __str__(self):
+        return '<%s>' % self.user.username
