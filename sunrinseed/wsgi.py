98353ccb788b9b4bee7b68b8c81a8b0f003a1ecb
DoHyun Kim <nero.union12@gmail.com>
2016-06-03 03:32:36 +0900

    For deploy

--- sunrinseed/wsgi.py
+++ sunrinseed/wsgi.py
@@ -9,7 +9,12 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 
 import os
 import site
+import sys
+
 from django.core.wsgi import get_wsgi_application
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
 
 site.addsitedir('/home/nero/.virtualenvs/sseed/lib/python3.4/site-packages')
 
