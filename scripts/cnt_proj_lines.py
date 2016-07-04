36ab84a0b98beff0c5d1a0bd193f8506a1c6bc88
DoHyun Kim <nero.union12@gmail.com>
2016-07-05 04:27:47 +0900

    asdf

---
+++ scripts/cnt_proj_lines.py
@@ -0,0 +1,22 @@
+import os
+from sunrinseed.settings.base import BASE_DIR
+
+
+def run():
+    dic = {'.html': 0, '.py': 0, '.css': 0, '.js': 0}
+    for (path, dir, files) in os.walk(BASE_DIR):
+        if 'env' in path or 'static' in path or '.git' in path or '.idea' in path:
+            continue
+        for filename in files:
+            ext = os.path.splitext(filename)[-1]
+            try:
+                num_lines = sum(1 for line in open(os.path.join(path, filename), encoding='utf-8'))
+            except UnicodeDecodeError:
+                continue
+            try:
+                dic[ext] += num_lines
+            except KeyError:
+                dic[ext] = num_lines
+            print(path, filename, num_lines)
+
+    print(dic, sum([(dic[k]) for k in dic.keys()]))
