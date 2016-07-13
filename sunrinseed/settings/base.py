e192e73f965d48268be1c61dde3be6fc30353a9f
DoHyun Kim <nero.union12@gmail.com>
2016-07-13 21:29:14 +0900

    테스트시 django 내부 DEBUG STRING 표시 안하도록

--- sunrinseed/settings/base.py
+++ sunrinseed/settings/base.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+import sys
+import logging
+
+if len(sys.argv) > 1 and sys.argv[1] == 'test':
+    logging.disable(logging.CRITICAL)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
