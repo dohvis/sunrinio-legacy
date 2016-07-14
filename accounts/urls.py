12a154ca040d18d1a791f326964b68c4e9cf0c08
Frank Yang <puilp0502@gmail.com>
2016-07-14 09:39:40 +0900

    Tidy up signup

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,5 +5,6 @@ from accounts import views
 urlpatterns = [
     url('^login/', views.login, name='login'),
     url('^logout/', views.logout, name='logout'),
+    url('^signup/', views.signup, name='signup'),
     url('^update_profile_image/', views.update_profile_image),
 ]
