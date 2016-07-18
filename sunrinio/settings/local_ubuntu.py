ac598fb9ba5a066b01380219f51c7be92abe3fdf
DoHyun Kim <nero.union12@gmail.com>
2016-07-18 17:41:21 +0900

    CI에서 테스트해야되니까 local_ubuntu는 추가해줌

---
+++ sunrinio/settings/local_ubuntu.py
@@ -0,0 +1,8 @@
+from sunrinio.settings.base import *  # NOQA
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
